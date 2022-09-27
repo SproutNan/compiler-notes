39acb75b383360ec551cf374d30809edac6f6b14
Sprout <sprout@mail.ustc.edu.cn>
2022-09-27 20:46:53 +0800

    Deployed 606e900 with MkDocs version: 1.3.1

---
+++ mathjax-config.js
@@ -0,0 +1,26 @@
+/* mathjax-loader.js  file */
+/* ref: http://facelessuser.github.io/pymdown-extensions/extensions/arithmatex/ */
+(function (win, doc) {
+    window.MathJax = {
+        loader: {
+          load: ['[tex]/tagformat']
+        },
+        startup: {
+          pageReady: () => {
+            alert('Running MathJax');
+            return MathJax.startup.defaultPageReady();
+          }
+        },
+        tex: {
+          packages: {'[+]': ['tagformat']},
+          tagSide: 'left',
+          macros: {
+            RR: '{\\bf R}',
+            bold: ['{\\bf #1}',1]
+          },
+          tagformat: {
+             tag: (n) => '[' + n + ']'
+          }
+        }
+      };
+  })(window, document);
