d63681fe03270dfdc3d4e098a58f1938dfb455c2
Sprout <sprout@mail.ustc.edu.cn>
2022-09-27 22:14:11 +0800

    Deployed 606e900 with MkDocs version: 1.3.1

---
+++ comment.js
@@ -0,0 +1,2 @@
+< script  src = " https://utteranc.es/client.js "
+ repo = " SproutNan/compiler-notes " issue-term = " pathname " theme = " github-light " crossorigin = " anonymous " async > </ script >
